0331df7adf5ff247a2818284fc24d55b06ff1eba
PimiYvan <ynjomkam@gmail.com>
2023-10-25 22:16:45 +0100

    first commit

---
+++ dataset.sh
@@ -0,0 +1,24 @@
+
+echo 'downloading pascal'
+!wget http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar
+!tar -xvf VOCtrainval_11-May-2012.tar
+
+!pip install gdown
+
+echo 'downloading segmentation class of pascal'
+!gdown 10zxG2VExoEZUeyQl_uXga2OWHjGeZaf2
+!unzip SegmentationClassAug.zip
+
+!mv SegmentationClassAug ./VOCdevkit/VOC2012/
+
+!mkdir Datasets_PATNET
+!mv ./VOCdevkit ./Datasets_PATNET
+
+echo 'downloading Fss dataset'
+!gdown 16TgqOeI_0P41Eh3jWQlxlRXG9KIqtMgI
+!unzip fewshot_data.zip
+
+!mv ./fewshot_data/* ./Datasets_PATNET
+!rm -r ./fewshot_data
+!mv ./Datasets_PATNET/fewshot_data ./Datasets_PATNET/FSS-1000
+
---
+++ note.md
@@ -0,0 +1,15 @@
+
+git clone https://github.com/PimiYvan/vi_local_server.git <br/>
+cd vi_local_server
+python3.10 -m venv env <br/>
+source env/bin/activate <br/>
+pip install --upgrade pip <br/>
+pip install -r requirements.txt <br/>
+
+
+
+if you installed a package you should run <br/>
+pip freeze > requirements.txt <br/>
+if you setting up the project you should run <br/>
+pip install -r requirements.txt <br/>
+
