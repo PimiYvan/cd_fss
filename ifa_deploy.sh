d392ec797e7620ac2423935728b1b778980bde87
PimiYvan <ynjomkam@gmail.com>
2024-05-01 11:41:44 +0100

    preprocess deepglobe

---
+++ ifa_deploy.sh
@@ -0,0 +1,10 @@
+
+
+git clone https://github.com/niejiahao1998/IFA.git <br/>
+chmod +x dataset.sh 
+cd cd_fss
+python3.10 -m venv env <br/> [mii] loading StdEnv/2023 python/3.10.13
+source env/bin/activate <br/>
+pip install --upgrade pip <br/>
+pip install -r requirements.txt <br/>
+
